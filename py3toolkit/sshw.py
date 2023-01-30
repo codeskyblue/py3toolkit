4801170dfd5d2ec85da7d1a176b69e9a80036bd7
codeskyblue <codeskyblue@gmail.com>
2023-01-30 19:11:49 +0800

    little update

--- py3toolkit/sshw.py
+++ py3toolkit/sshw.py
@@ -7,18 +7,36 @@ Clone of [sshw](https://github.com/yinheli/sshw) written in Python
 
 Refs:
 - https://python-prompt-toolkit.readthedocs.io/en/master/
+- sshpass in python https://gist.github.com/jlinoff/bdd346ffadc226337949
 """
 
-from prompt_toolkit import Application, HTML, print_formatted_text
-from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
-from prompt_toolkit.keys import Keys
+import typing
+from dataclasses import dataclass
+
+from dataclasses_json import DataClassJsonMixin
+from prompt_toolkit import HTML, Application, print_formatted_text
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.formatted_text import FormattedText
-from prompt_toolkit.styles import Style
-from prompt_toolkit.layout.containers import VSplit, HSplit, FloatContainer, Window
+from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.layout import VerticalAlign
+from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
-from prompt_toolkit.layout import VerticalAlign
+from prompt_toolkit.styles import Style
+
+
+@dataclass
+class HostConfig(DataClassJsonMixin):
+    name: str
+    user: str
+    host: str
+    port: int = 22
+    keypath: typing.Optional[str] = None
+    password: typing.Optional[str] = None
+    callback_shells: typing.Optional[typing.List[str]] = None
+    children: typing.Optional[typing.List['HostConfig']] = None
+
 
 # The style sheet.
 style = Style.from_dict({
@@ -42,9 +60,19 @@ def exit_(event: KeyPressEvent):
 host_names = ["root@20220122", "pi@raspberry", "dummy"]
 
 class SelectContainer(HSplit):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, host_configs: typing.List[HostConfig]):
+        self._host_configs = host_configs
         self._active_index = 0
+        super().__init__(self._gen_host_windows())
+    
+    def _gen_host_windows(self):
+        host_windows = []
+        for index, config in enumerate(self._host_configs):
+            if self._active_index == index:
+                host_windows.append(Window(content=FormattedTextControl(HTML(f"  ➤ <cyan>{config.name}</cyan> <gray>{config.user}@{config.host}</gray>")), height=1))
+            else:
+                host_windows.append(Window(content=FormattedTextControl(HTML(f"    <gray>{config.name} {config.user}@{config.host}</gray>")), height=1))
+        return host_windows
 
     def _up_hook(self, event: KeyPressEvent):
         index = self.get_active_index()
@@ -56,7 +84,7 @@ class SelectContainer(HSplit):
     
     def _enter_hook(self, event: KeyPressEvent):
         index = self.get_active_index()
-        event.app.exit(index)
+        event.app.exit(self._host_configs[index])
     
     def get_active_index(self):
         return self._active_index
@@ -66,11 +94,7 @@ class SelectContainer(HSplit):
         self._update_active()
     
     def _update_active(self):
-        for i, child in enumerate(self.children):
-            if i == self._active_index:
-                child.style = 'class:active'
-            else:
-                child.style = ''
+        self.children = self._gen_host_windows()
 
     def register_key_bindings(self, kb: KeyBindings):
         for key in ['up', 'k']:
@@ -81,26 +105,34 @@ class SelectContainer(HSplit):
 
 buffer1 = Buffer()  # Editable buffer.
 
-menu_items = []
-for name in host_names:
-    menu_items.append(Window(content=FormattedTextControl(HTML("> " + name)), height=1))
-menu_items[0].style = 'class:active'
 
-hosts_container = SelectContainer(menu_items)
-hosts_container.register_key_bindings(kb)
-
-
-root_container = HSplit([
-    Window(content=FormattedTextControl(HTML('<gray>Use the arrow keys to navigate: ↓ ↑ → ←  and / toggles search</gray>')), height=1),
-    Window(content=FormattedTextControl(HTML('<lightgreen>✨ Select host</lightgreen>')), height=1),
-    hosts_container,
-])
-
-layout = Layout(root_container)
+mymac = HostConfig(name="mymac", user="codeskyblue", host="127.0.0.1")
+pc001 = HostConfig(name="pc001", user="anonymous", host="example.com")
+pc002 = HostConfig(name="pc002", user="kitty", host="kitty.example.com")
 
 def main():
+    hosts_container = SelectContainer([mymac, pc001, pc002])
+    hosts_container.register_key_bindings(kb)
+
+    root_container = HSplit([
+        Window(content=FormattedTextControl(HTML('<gray>Use the arrow keys to navigate: ↓ ↑ → ←  and / toggles search</gray>')), height=1),
+        Window(content=FormattedTextControl(HTML('<lightgreen>✨ Select host</lightgreen>')), height=1),
+        hosts_container,
+    ])
+
+    layout = Layout(root_container)
+
     app = Application(layout=layout, style=style, key_bindings=kb, full_screen=True)
-    print(app.run())
+    host_config = app.run()
+    if host_config:
+        print(host_config)
+        cmds = ["ssh"]
+        if host_config.port != 22:
+            cmds.extend(["-p", str(host_config.port)])
+        cmds.extend([f"{host_config.user}@{host_config.host}"])
+        if host_config.password:
+            cmds.insert(0, ["sshpass", "-p", host_config.password])
+        print(" ".join(cmds))
 
 
 if __name__ == '__main__':
