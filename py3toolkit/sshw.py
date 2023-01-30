7a9b3da00fcf9d0a34b617a0ab3fbbd76d2e0f2f
codeskyblue <codeskyblue@gmail.com>
2023-01-30 23:00:48 +0800

    add children jump support

--- py3toolkit/sshw.py
+++ py3toolkit/sshw.py
@@ -10,13 +10,15 @@ Refs:
 - sshpass in python https://gist.github.com/jlinoff/bdd346ffadc226337949
 """
 
+import dataclasses
+import yaml
+import pathlib
 import typing
 from dataclasses import dataclass
 
-from dataclasses_json import DataClassJsonMixin
+from dataclasses_json import DataClassJsonMixin, config as dconfig
 from prompt_toolkit import HTML, Application, print_formatted_text
 from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
 from prompt_toolkit.keys import Keys
 from prompt_toolkit.layout import VerticalAlign
@@ -29,18 +31,24 @@ from prompt_toolkit.styles import Style
 @dataclass
 class HostConfig(DataClassJsonMixin):
     name: str
-    user: str
-    host: str
     port: int = 22
+    host: typing.Optional[str] = None
+    user: typing.Optional[str] = None
     keypath: typing.Optional[str] = None
-    password: typing.Optional[str] = None
-    callback_shells: typing.Optional[typing.List[str]] = None
-    children: typing.Optional[typing.List['HostConfig']] = None
+    password: typing.Optional[typing.Union[str, int]] = None
+    callback_shells: typing.Optional[typing.List[dict]] = dataclasses.field(metadata=dconfig(field_name="callback-shells"), default=None)
+    children: typing.Optional[typing.List["HostConfig"]] = None
+
+    def get_password(self) -> typing.Optional[str]:
+        if self.password is None:
+            return None
+        return str(self.password)
 
 
 # The style sheet.
 style = Style.from_dict({
     'active': 'red',
+    'name': 'cyan',
     'bbb': '#44ff00 italic',
 })
 
@@ -68,10 +76,12 @@ class SelectContainer(HSplit):
     def _gen_host_windows(self):
         host_windows = []
         for index, config in enumerate(self._host_configs):
+            prefix = "" if config.children else f" <gray>{config.user}@{config.host}</gray>"
             if self._active_index == index:
-                host_windows.append(Window(content=FormattedTextControl(HTML(f"  ➤ <cyan>{config.name}</cyan> <gray>{config.user}@{config.host}</gray>")), height=1))
+                html_text = "  ➤ " + f"<name>{config.name}</name>" + prefix
             else:
-                host_windows.append(Window(content=FormattedTextControl(HTML(f"    <gray>{config.name} {config.user}@{config.host}</gray>")), height=1))
+                html_text = "    " + f"<gray>{config.name}</gray>" + prefix
+            host_windows.append(Window(content=FormattedTextControl(HTML(html_text)), height=1))
         return host_windows
 
     def _up_hook(self, event: KeyPressEvent):
@@ -84,7 +94,16 @@ class SelectContainer(HSplit):
     
     def _enter_hook(self, event: KeyPressEvent):
         index = self.get_active_index()
-        event.app.exit(self._host_configs[index])
+        config = self._host_configs[index]
+        if config.children:
+            if config.name == "-parent-":
+                self._host_configs = config.children
+            else:
+                parent = HostConfig(name="-parent-", children=self._host_configs[:])
+                self._host_configs = [parent] + config.children
+            self.set_active_index(0)
+        else:
+            event.app.exit(config)
     
     def get_active_index(self):
         return self._active_index
@@ -110,8 +129,30 @@ mymac = HostConfig(name="mymac", user="codeskyblue", host="127.0.0.1")
 pc001 = HostConfig(name="pc001", user="anonymous", host="example.com")
 pc002 = HostConfig(name="pc002", user="kitty", host="kitty.example.com")
 
+test_config = """
+- name: mymac
+  password: hahaha
+  children:
+    - name: pc001
+      user: anonymous
+      host: example.com
+    - name: pc002
+      user: kitty
+      password: 123123
+      host: kitty.example.com
+      callback-shells:
+      - {cmd: "echo hello world"}
+"""
+
+def load_config() -> typing.List[HostConfig]:
+    p = pathlib.Path("~/.sshw.yml").expanduser()
+    return HostConfig.schema().load(yaml.safe_load(p.read_bytes()), many=True)
+    # return HostConfig.schema().load(yaml.safe_load(test_config), many=True)
+
+
 def main():
-    hosts_container = SelectContainer([mymac, pc001, pc002])
+    host_configs = load_config()
+    hosts_container = SelectContainer(host_configs) #[mymac, pc001, pc002])
     hosts_container.register_key_bindings(kb)
 
     root_container = HSplit([
@@ -130,8 +171,9 @@ def main():
         if host_config.port != 22:
             cmds.extend(["-p", str(host_config.port)])
         cmds.extend([f"{host_config.user}@{host_config.host}"])
-        if host_config.password:
-            cmds.insert(0, ["sshpass", "-p", host_config.password])
+        if host_config.get_password():
+            cmds = ["sshpass", "-p", host_config.get_password()] + cmds
+        print(cmds)
         print(" ".join(cmds))
 
 
